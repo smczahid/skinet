982a88d55eb76f9f9705f854b4d4ed26a01a5e06
Zahid Alware <zahidalware@gmail.com>
2022-03-25 01:13:56 +0530

    End of seciton 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,12 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 // Add services to the container.
 
@@ -15,7 +19,28 @@ builder.Services.AddDbContext<StoreContext>(x=>
     x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope()){
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context,loggerFactory);
+    }
+    catch(Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occured during migration");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
